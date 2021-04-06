9694e051a14125377e8c8cf51aeed24e897c6369
Mikel Pardo <mikel.pardo@biko2.com>
2021-04-06 16:22:18 +0200

    Apply change

--- src/ui/views/Home/Home.jsx
+++ src/ui/views/Home/Home.jsx
@@ -3,6 +3,7 @@ import { MobileList, MobileListWrapper } from 'ui/views/Home/Home.styles'
 import { Searcher } from 'ui/views/Home/_components/Searcher/Searcher.jsx'
 import { MobileListItem } from 'ui/views/Home/_components/MobileListItem/MobileListItem.jsx'
 import { mobileService } from 'core/services/Mobile'
+import { Text } from 'ui/components/Text/Text'
 
 export const Home = () => {
   const [mobileList, setMobileList] = useState([])
@@ -31,6 +32,7 @@ export const Home = () => {
 
   return (
     <MobileListWrapper>
+      <Text>Pre production change</Text>
       <Searcher onChange={onSearchValueChange} />
       <MobileList>
         {getMobileList().map(mobile => (
